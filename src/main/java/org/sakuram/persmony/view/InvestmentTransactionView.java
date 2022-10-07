48957b890367bad7a26044e8d84405a59e525c20
jofatmofn <jofatmofn@users.noreply.github.com>
2022-10-07 14:37:03 +0530

    UI for the Invest Operation.

--- src/main/java/org/sakuram/persmony/view/InvestmentTransactionView.java
+++ src/main/java/org/sakuram/persmony/view/InvestmentTransactionView.java
@@ -10,6 +10,7 @@ import org.sakuram.persmony.util.AppException;
 import org.sakuram.persmony.util.Constants;
 import org.sakuram.persmony.util.UtilFuncs;
 import org.sakuram.persmony.valueobject.IdValueVO;
+import org.sakuram.persmony.valueobject.InvestVO;
 import org.sakuram.persmony.valueobject.RenewalVO;
 import org.sakuram.persmony.valueobject.ScheduleVO;
 import org.sakuram.persmony.valueobject.SingleRealisationWithBankVO;
@@ -26,6 +27,7 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -276,6 +278,208 @@ public class InvestmentTransactionView extends Div {
 	}
 	
 	private void handleInvest(FormLayout formLayout) {
+		Select<IdValueVO> investorDvSelect, productProviderDvSelect, productTypeDvSelect, dematAccountDvSelect, taxabilityDvSelect, bankAccountDvSelect;
+		TextField productIdOfProviderTextField, investorIdWithProviderTextField, productNameTextField, investmentIdWithProviderTextField, paymentScheduleTextField, receiptScheduleTextField, accrualScheduleTextField;
+		RadioButtonGroup<String> accrualApplicabilityRadioButtonGroup;
+		NumberField rateOfInterestNumberField;
+		DatePicker productEndDatePicker;
+		List<IdValueVO> idValueVOList;
+		
+		Button saveButton;
+		
+		// UI Elements
+		investorDvSelect = new Select<IdValueVO>();
+		formLayout.addFormItem(investorDvSelect, "Investor");
+		idValueVOList = miscService.fetchDvsOfCategory(Constants.CATEGORY_INVESTOR);
+		investorDvSelect.setItemLabelGenerator(idValueVO -> {
+			return idValueVO.getValue();
+		});
+		investorDvSelect.setItems(idValueVOList);
+		investorDvSelect.setPlaceholder("Select Investor");
+
+		productProviderDvSelect = new Select<IdValueVO>();
+		formLayout.addFormItem(productProviderDvSelect, "Product Provider");
+		idValueVOList = miscService.fetchDvsOfCategory(Constants.CATEGORY_PARTY);
+		productProviderDvSelect.setItemLabelGenerator(idValueVO -> {
+			return idValueVO.getValue();
+		});
+		productProviderDvSelect.setItems(idValueVOList);
+		productProviderDvSelect.setPlaceholder("Select Product Provider");
+		
+		productIdOfProviderTextField = new TextField();
+		formLayout.addFormItem(productIdOfProviderTextField, "Product Id of Provider");
+
+		investorIdWithProviderTextField = new TextField();
+		formLayout.addFormItem(investorIdWithProviderTextField, "Investor Id with Provider");
+
+		productNameTextField = new TextField();
+		formLayout.addFormItem(productNameTextField, "Product Name");
+
+		productTypeDvSelect = new Select<IdValueVO>();
+		formLayout.addFormItem(productTypeDvSelect, "Product Type");
+		idValueVOList = miscService.fetchDvsOfCategory(Constants.CATEGORY_PRODUCT_TYPE);
+		productTypeDvSelect.setItemLabelGenerator(idValueVO -> {
+			return idValueVO.getValue();
+		});
+		productTypeDvSelect.setItems(idValueVOList);
+		productTypeDvSelect.setPlaceholder("Select Product Type");
+		
+		dematAccountDvSelect = new Select<IdValueVO>();
+		formLayout.addFormItem(dematAccountDvSelect, "Demat Account");
+		idValueVOList = miscService.fetchDvsOfCategory(Constants.CATEGORY_DEMAT_ACCOUNT);
+		dematAccountDvSelect.setItemLabelGenerator(idValueVO -> {
+			return idValueVO.getValue();
+		});
+		dematAccountDvSelect.setItems(idValueVOList);
+		dematAccountDvSelect.setPlaceholder("Select Demat Account");
+		
+		taxabilityDvSelect = new Select<IdValueVO>();
+		formLayout.addFormItem(taxabilityDvSelect, "Taxability");
+		idValueVOList = miscService.fetchDvsOfCategory(Constants.CATEGORY_TAXABILITY);
+		taxabilityDvSelect.setItemLabelGenerator(idValueVO -> {
+			return idValueVO.getValue();
+		});
+		taxabilityDvSelect.setItems(idValueVOList);
+		taxabilityDvSelect.setPlaceholder("Select Taxability");
+		
+		accrualApplicabilityRadioButtonGroup = new RadioButtonGroup<>();
+		formLayout.addFormItem(accrualApplicabilityRadioButtonGroup, "Accrual Applicability");
+		// accrualApplicabilityRadioButtonGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+		accrualApplicabilityRadioButtonGroup.setItems("Not Known", "Not Applicable", "Applicable");
+		accrualApplicabilityRadioButtonGroup.setValue("Not Known");
+		
+		bankAccountDvSelect = new Select<IdValueVO>();
+		formLayout.addFormItem(bankAccountDvSelect, "Investment from Bank Account");
+		idValueVOList = miscService.fetchDvsOfCategory(Constants.CATEGORY_BANK_ACCOUNT);
+		bankAccountDvSelect.setItemLabelGenerator(idValueVO -> {
+			return idValueVO.getValue();
+		});
+		bankAccountDvSelect.setItems(idValueVOList);
+		bankAccountDvSelect.setPlaceholder("Select Bank Account");
+
+		investmentIdWithProviderTextField = new TextField();
+		formLayout.addFormItem(investmentIdWithProviderTextField, "Investment Id with Provider");
+
+		rateOfInterestNumberField = new NumberField();
+		rateOfInterestNumberField.setMax(100.00);
+		formLayout.addFormItem(rateOfInterestNumberField, "Rate Of Interest%");
+		
+		productEndDatePicker = new DatePicker();
+		formLayout.addFormItem(productEndDatePicker, "Product End Date");
+		
+		paymentScheduleTextField = new TextField();
+		formLayout.addFormItem(paymentScheduleTextField, "Payment Schedule");
+		
+		receiptScheduleTextField = new TextField();
+		receiptScheduleTextField.setValue("None");
+		formLayout.addFormItem(receiptScheduleTextField, "Receipt Schedule");
+		
+		accrualScheduleTextField = new TextField();
+		accrualScheduleTextField.setValue("None");
+		formLayout.addFormItem(accrualScheduleTextField, "Accrual Schedule");
+		
+		saveButton = new Button("Save");
+		formLayout.add(saveButton);
+		saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		saveButton.setDisableOnClick(true);
+		// On click of Save
+		saveButton.addClickListener(event -> {
+			InvestVO investVO;
+			Notification notification;
+			List<ScheduleVO> paymentScheduleVOList;
+			List<ScheduleVO> receiptScheduleVOList;
+			List<ScheduleVO> accrualScheduleVOList;
+
+			try {
+				// Validation
+				if (investorDvSelect.getValue() == null) {
+					showError("Investor cannot be Empty");
+					return;
+				}
+				if (productProviderDvSelect.getValue() == null) {
+					showError("Product Provider cannot be Empty");
+					return;
+				}
+				if (productTypeDvSelect.getValue() == null) {
+					showError("Product Type cannot be Empty");
+					return;
+				}
+				if (bankAccountDvSelect.getValue() == null) {
+					showError("Bank Account cannot be Empty");
+					return;
+				}
+				if (investmentIdWithProviderTextField.getValue() == null || investmentIdWithProviderTextField.getValue().equals("")) {
+					showError("Investment Id With Provider cannot be Empty");
+					return;
+				}				
+				if (productEndDatePicker.getValue() == null) {
+					showError("Product End Date cannot be Empty");
+					return;
+				}
+				if (paymentScheduleTextField.getValue() == null || paymentScheduleTextField.getValue().equals("")) {
+					showError("Payment Schedule cannot be Empty");
+					return;
+				}
+				try {
+					paymentScheduleVOList = UtilFuncs.parseScheduleData(paymentScheduleTextField.getValue());
+				} catch (AppException e) {
+					showError("Payment Schedule: " + e.getMessage());
+					return;
+				}
+				if (paymentScheduleVOList.isEmpty()) {
+					showError("Payment Schedule cannot be Empty");
+					return;
+				}
+				if (receiptScheduleTextField.getValue() == null || receiptScheduleTextField.getValue().equals("")) {
+					showError("Specify None if there is no Receipt Schedule");
+					return;
+				}
+				try {
+					receiptScheduleVOList = UtilFuncs.parseScheduleData(receiptScheduleTextField.getValue());
+				} catch (AppException e) {
+					showError("Receipt Schedule: " + e.getMessage());
+					return;
+				}
+				if (accrualScheduleTextField.getValue() == null || accrualScheduleTextField.getValue().equals("")) {
+					showError("Specify None if there is no Accrual Schedule");
+					return;
+				}
+				try {
+					accrualScheduleVOList = UtilFuncs.parseScheduleData(accrualScheduleTextField.getValue());
+				} catch (AppException e) {
+					showError("Accrual Schedule: " + e.getMessage());
+					return;
+				}
+				
+				// Back-end Call
+				investVO = new InvestVO(
+						investorDvSelect.getValue().getId(),
+						productProviderDvSelect.getValue().getId(),
+						productIdOfProviderTextField.getValue().equals("") ? null : productIdOfProviderTextField.getValue(),
+						investorIdWithProviderTextField.getValue().equals("") ? null : investorIdWithProviderTextField.getValue(),
+						productNameTextField.getValue().equals("") ? null : productNameTextField.getValue(),
+						productTypeDvSelect.getValue().getId(),
+						dematAccountDvSelect.getValue() == null ? null : dematAccountDvSelect.getValue().getId(),
+						taxabilityDvSelect.getValue() == null ? null : taxabilityDvSelect.getValue().getId(),
+						(accrualApplicabilityRadioButtonGroup.getValue() == null || accrualApplicabilityRadioButtonGroup.getValue().equals("Not Known")) ? null : (accrualApplicabilityRadioButtonGroup.getValue().equals("Not Applicable") ? false : true),
+						bankAccountDvSelect.getValue().getId(),
+						investmentIdWithProviderTextField.getValue(),
+						rateOfInterestNumberField.getValue() == null ? null : (float)rateOfInterestNumberField.getValue().doubleValue(),
+						Date.valueOf(productEndDatePicker.getValue()),
+						paymentScheduleVOList,
+						receiptScheduleVOList,
+						accrualScheduleVOList);
+				try {
+					moneyTransactionService.invest(investVO);
+					notification = Notification.show("Investment Created Successfully.");
+					notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+				} catch (Exception e) {
+					showError(messageFromException(e));
+				}
+			} finally {
+				saveButton.setEnabled(true);
+			}
+		});
 	}
 	
 	private void handleRenewal(FormLayout formLayout) {
