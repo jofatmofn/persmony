f3f368138ffd7ddecafbc7902e2dc7129f9ede6b
Suresh Kumar Ramasamy <jofatmofn@users.noreply.github.com>
2025-07-24 16:46:52 +0530

    In Investments CSV, enclose Provider Branch within quotes as the value can have comma. When matching for FIFO purpose, include trade in the FROM side (ISIN Action alone is not sufficient).

--- src/main/java/org/sakuram/persmony/bean/IsinActionMatch.java
+++ src/main/java/org/sakuram/persmony/bean/IsinActionMatch.java
@@ -1,32 +1,18 @@
 package org.sakuram.persmony.bean;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-@Embeddable
-class IsinActionMatchKey implements Serializable {
-
-	private static final long serialVersionUID = -7261761931493608663L;
-
-	@Column(name="from_isin_action_fk")
-    long fromIsinActionFk;
-
-    @Column(name="to_isin_action_fk")
-    long toIsinActionFk;
-
-}
 
 @Getter @Setter
 @NoArgsConstructor
@@ -34,16 +20,21 @@ class IsinActionMatchKey implements Serializable {
 @Table(name="isin_action_match")
 public class IsinActionMatch {
 
-    @EmbeddedId
-    IsinActionMatchKey id;
-    
+	@Id
+	@SequenceGenerator(name="isin_action_match_seq_generator",sequenceName="isin_action_match_seq", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="isin_action_match_seq_generator")
+	@Column(name="id", nullable=false)
+	private long id;
+
 	@ManyToOne
-	@MapsId("fromIsinActionFk")
 	@JoinColumn(name="from_isin_action_fk", nullable=false)
 	private IsinAction fromIsinAction;
 	
 	@ManyToOne
-	@MapsId("toIsinActionFk")
+	@JoinColumn(name="from_trade_fk", nullable=true)
+	private Trade fromTrade;
+	
+	@ManyToOne
 	@JoinColumn(name="to_isin_action_fk", nullable=false)
 	private IsinAction toIsinAction;
 	
