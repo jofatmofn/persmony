031e2350af745dc82dadb38c513dd3d875132d04
Suresh Kumar Ramasamy <jofatmofn@users.noreply.github.com>
2025-08-20 17:44:57 +0530

    Refactoring the "Security History and Balances" functionality into a new "Security Search" (UI) component.

--- src/main/java/org/sakuram/persmony/view/DebtEquityMutualView.java
+++ src/main/java/org/sakuram/persmony/view/DebtEquityMutualView.java
@@ -4,19 +4,13 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.sakuram.persmony.service.DebtEquityMutualService;
 import org.sakuram.persmony.service.MiscService;
 import org.sakuram.persmony.util.Constants;
-import org.sakuram.persmony.util.UtilFuncs;
-import org.sakuram.persmony.valueobject.FieldSpecVO;
 import org.sakuram.persmony.valueobject.IdValueVO;
 import org.sakuram.persmony.valueobject.IsinActionVO;
-import org.sakuram.persmony.valueobject.IsinCriteriaVO;
-import org.sakuram.persmony.valueobject.IsinVO;
-
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
@@ -24,16 +18,13 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.Column;
-import com.vaadin.flow.component.grid.contextmenu.GridContextMenu;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("dem")
@@ -58,7 +49,9 @@ public class DebtEquityMutualView extends Div {
 			private static final long serialVersionUID = 1L;
 
 			{
-				add(new AbstractMap.SimpleImmutableEntry<Integer, String>(1, "Search Security"));
+				add(new AbstractMap.SimpleImmutableEntry<Integer, String>(1, "Security History"));
+				add(new AbstractMap.SimpleImmutableEntry<Integer, String>(2, "Create ISIN Action"));
+				add(new AbstractMap.SimpleImmutableEntry<Integer, String>(3, "Match ISIN Actions"));
 				add(new AbstractMap.SimpleImmutableEntry<Integer, String>(2, "Test WIP"));
 			}
 		};
@@ -84,6 +77,12 @@ public class DebtEquityMutualView extends Div {
 	            	break;
 	            case 2:
 	            	break;
+	            case 3:
+	            	handleMatchIsinActions(formLayout);
+	            	break;
+	            case 4:
+	            	debtEquityMutualService.determineBalancesMultiple("INE081A01012", new java.sql.Date(System.currentTimeMillis()), null);
+	            	break;
 	            }
 			} catch (Exception e) {
 				ViewFuncs.showError("System Error!!! Contact Support.");
@@ -98,193 +97,98 @@ public class DebtEquityMutualView extends Div {
 	}
 	
 	private void handleSearchSecurity(FormLayout formLayout) {
-		TextField isinTextField, companyNameTextField, securityNameTextField;
-		Select<IdValueVO> isinOperatorSelect, companyNameOperatorSelect, securityNameOperatorSelect;
-		Select<IdValueVO> securityTypeDvSelect;
 		HorizontalLayout hLayout;
-		Button fetchButton;
-		Grid<IsinVO> isinsGrid;
-		GridContextMenu<IsinVO> isinsGridContextMenu;
+		Button historyButton, balancesButton;
 		Grid<IsinActionVO> isinActionsGrid;
+		SecuritySearchComponent securitySearchComponent;
 		
-		isinOperatorSelect = ViewFuncs.newSelect(FieldSpecVO.getTxtOperatorList(), "Operator", true, false);
-		isinTextField = new TextField("Value");
+		securitySearchComponent = new SecuritySearchComponent(debtEquityMutualService, miscService);
+		formLayout.addFormItem(securitySearchComponent.getLayout(), "ISIN");
+		
 		hLayout = new HorizontalLayout();
-		formLayout.addFormItem(hLayout, "ISIN");
-		hLayout.add(isinOperatorSelect, isinTextField);
-		isinOperatorSelect.addValueChangeListener(event -> {
-			if (isinOperatorSelect.getValue() == null) {
-				isinTextField.setValue("");
-			}
-		});
+		formLayout.add(hLayout);
+		historyButton = new Button("History");
+		hLayout.add(historyButton);
+		historyButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		historyButton.setDisableOnClick(true);
 		
-		companyNameOperatorSelect = ViewFuncs.newSelect(FieldSpecVO.getTxtOperatorList(), "Operator", true, false);
-		companyNameTextField = new TextField("Value");
-		hLayout = new HorizontalLayout();
-		formLayout.addFormItem(hLayout, "Company Name");
-		hLayout.add(companyNameOperatorSelect, companyNameTextField);
-		companyNameOperatorSelect.addValueChangeListener(event -> {
-			if (companyNameOperatorSelect.getValue() == null) {
-				companyNameTextField.setValue("");
-			}
-		});
-		
-		securityNameOperatorSelect = ViewFuncs.newSelect(FieldSpecVO.getTxtOperatorList(), "Operator", true, false);
-		securityNameTextField = new TextField("Value");
-		hLayout = new HorizontalLayout();
-		formLayout.addFormItem(hLayout, "Security Name");
-		hLayout.add(securityNameOperatorSelect, securityNameTextField);
-		securityNameOperatorSelect.addValueChangeListener(event -> {
-			if (securityNameOperatorSelect.getValue() == null) {
-				securityNameTextField.setValue("");
-			}
-		});
-		
-		securityTypeDvSelect = ViewFuncs.newDvSelect(miscService, Constants.CATEGORY_SECURITY_TYPE, null, true, false);
-		formLayout.addFormItem(securityTypeDvSelect, "Security Type");
-		
-		fetchButton = new Button("Fetch");
-		formLayout.add(fetchButton);
-		fetchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		fetchButton.setDisableOnClick(true);
-		
-		isinsGrid = new Grid<>(IsinVO.class);
-		isinsGrid.setColumns("isin", "companyName", "securityName", "securityType");
-		for (Column<IsinVO> column : isinsGrid.getColumns()) {
-			column.setResizable(true);
-		}
-		formLayout.add(isinsGrid);
-		formLayout.add(new Label(" "));
+		balancesButton = new Button("Balances");
+		hLayout.add(balancesButton);
+		balancesButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		balancesButton.setDisableOnClick(true);
 		
 		isinActionsGrid = new Grid<>(IsinActionVO.class);
 		formLayout.add(isinActionsGrid);
 		
-		// On click of Fetch
-		fetchButton.addClickListener(event -> {
-			IsinCriteriaVO isinCriteriaVO;
-			List<IsinVO> isinVOList = null;
-			Notification notification;
-
+		historyButton.addClickListener(event -> {
 			try {
-				// Validation
-				if (!isinTextField.isEmpty() && isinOperatorSelect.getValue() == null) {
-					ViewFuncs.showError("ISIN: Non-matching Operator and Value");
+		        Dialog dialog;
+				Select<IdValueVO> dematAccountDvSelect;
+				Button proceedButton;
+		        
+				if (securitySearchComponent.getIsinTextField() == null || securitySearchComponent.getIsinTextField().isEmpty()) {
 					return;
 				}
-				if (isinTextField.isEmpty() && isinOperatorSelect.getValue() != null) {
-					ViewFuncs.showError("Specify Value for ISIN");
-					return;
-				}
-				if (!companyNameTextField.isEmpty() && companyNameOperatorSelect.getValue() == null) {
-					ViewFuncs.showError("Company Name: Non-matching Operator and Value");
-					return;
-				}
-				if (companyNameTextField.isEmpty() && companyNameOperatorSelect.getValue() != null) {
-					ViewFuncs.showError("Specify Value for Company Name");
-					return;
-				}
-				if (!securityNameTextField.isEmpty() && securityNameOperatorSelect.getValue() == null) {
-					ViewFuncs.showError("Security Name: Non-matching Operator and Value");
-					return;
-				}
-				if (securityNameTextField.isEmpty() && securityNameOperatorSelect.getValue() != null) {
-					ViewFuncs.showError("Specify Value for Security Name");
-					return;
-				}
-				
-				// Back-end Call
-				isinCriteriaVO = new IsinCriteriaVO(
-						isinTextField.getValue().equals("") ? null : isinTextField.getValue(),
-						isinOperatorSelect.getValue() == null ? null : isinOperatorSelect.getValue().getValue(),
-						companyNameTextField.getValue().equals("") ? null : companyNameTextField.getValue(),
-						companyNameOperatorSelect.getValue() == null ? null : companyNameOperatorSelect.getValue().getValue(),
-						securityNameTextField.getValue().equals("") ? null : securityNameTextField.getValue(),
-						securityNameOperatorSelect.getValue() == null ? null : securityNameOperatorSelect.getValue().getValue(),
-						securityTypeDvSelect.getValue() == null ? null : securityTypeDvSelect.getValue().getId()
-						);
-				try {
-					isinVOList = debtEquityMutualService.searchSecurities(isinCriteriaVO);
-					notification = Notification.show("No. of ISINs fetched: " + isinVOList.size());
-					notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-					isinsGrid.setItems(isinVOList);
-					isinActionsGrid.setItems(new ArrayList<IsinActionVO>());
-				} catch (Exception e) {
-					ViewFuncs.showError(UtilFuncs.messageFromException(e));
-					return;
-				}
-			} finally {
-				fetchButton.setEnabled(true);
-			}
-		});
-		
-		isinsGridContextMenu = isinsGrid.addContextMenu();
-		isinsGridContextMenu.addItem("History", event -> {
-			Optional<IsinVO> isinVO;
-			
-			isinVO = event.getItem();
-			if (isinVO.isPresent()) {
-				try {
-				        Dialog dialog;
-						Select<IdValueVO> dematAccountDvSelect;
-						Button proceedButton;
-				        
-				        dialog = new Dialog();
-				        dialog.setModal(true); // Non-modal popover effect
-				        dialog.setDraggable(true);
-				        dialog.setCloseOnOutsideClick(true);
-						dialog.setHeaderTitle("Demat Account");
-				        
-				        dematAccountDvSelect = ViewFuncs.newDvSelect(miscService.fetchDvsOfCategory(Constants.CATEGORY_DEMAT_ACCOUNT, true, false), "Demat Account", true, false);
+		        dialog = new Dialog();
+		        dialog.setModal(true); // Non-modal popover effect
+		        dialog.setDraggable(true);
+		        dialog.setCloseOnOutsideClick(true);
+				dialog.setHeaderTitle("Demat Account");
+		        
+		        dematAccountDvSelect = ViewFuncs.newDvSelect(miscService.fetchDvsOfCategory(Constants.CATEGORY_DEMAT_ACCOUNT, true, false), "Demat Account", true, false);
 
-				        proceedButton = new Button("Proceed", e -> {
-							List<IsinActionVO> isinActionVOList = null;
-							Notification notification;
-							
-				            dialog.close();
-							isinActionVOList = debtEquityMutualService.fetchIsinActions(isinVO.get().getIsin(), new java.sql.Date(new java.util.Date().getTime()), dematAccountDvSelect.getValue() == null ? null : dematAccountDvSelect.getValue().getId(), true);
-							notification = Notification.show("No. of ISIN Actions fetched: " + isinActionVOList.size());
-							notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-							isinActionsGrid.setColumns("settlementDate", "isin", "securityName", "isinActionId", "tradeId", "actionType", "bookingType", "dematAccount", "transactionQuantity");
-							for (Column<IsinActionVO> column : isinActionsGrid.getColumns()) {
-								column.setResizable(true);
-							}
-							isinActionsGrid.setItems(isinActionVOList);
-				        });
-
-				        dialog.add(new VerticalLayout(dematAccountDvSelect, proceedButton));
-				        dialog.open();
-
-				} catch (Exception e) {
-					ViewFuncs.showError("System Error!!! Contact Support.");
-					e.printStackTrace();
-					return;
-				}
-			}
-		});
-		isinsGridContextMenu.addItem("Balances", event -> {
-			Optional<IsinVO> isinVO;
-			
-			isinVO = event.getItem();
-			if (isinVO.isPresent()) {
-				try {
+		        proceedButton = new Button("Proceed", e -> {
 					List<IsinActionVO> isinActionVOList = null;
-					Notification notification;
 					
-					isinActionVOList = debtEquityMutualService.determineBalancesMultiple(isinVO.get().getIsin(), new java.sql.Date(new java.util.Date().getTime()), null);
-					notification = Notification.show("No. of ISIN Actions with Balances: " + isinActionVOList.size());
-					notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-					isinActionsGrid.setColumns("settlementDate", "isin", "securityName", "isinActionId", "tradeId", "actionType", "bookingType", "dematAccount", "balance", "ppuBalance");
+		            dialog.close();
+					isinActionVOList = debtEquityMutualService.fetchIsinActions(securitySearchComponent.getIsinTextField().getValue(), new java.sql.Date(new java.util.Date().getTime()), dematAccountDvSelect.getValue() == null ? null : dematAccountDvSelect.getValue().getId(), true);
+					Notification.show("No. of ISIN Actions fetched: " + isinActionVOList.size())
+						.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+					isinActionsGrid.setColumns("settlementDate", "isin", "securityName", "isinActionId", "tradeId", "actionType", "bookingType", "dematAccount", "transactionQuantity");
 					for (Column<IsinActionVO> column : isinActionsGrid.getColumns()) {
 						column.setResizable(true);
 					}
 					isinActionsGrid.setItems(isinActionVOList);
-				} catch (Exception e) {
-					ViewFuncs.showError("System Error!!! Contact Support.");
-					e.printStackTrace();
-					return;
-				}
+		        });
+
+		        dialog.add(new VerticalLayout(dematAccountDvSelect, proceedButton));
+		        dialog.open();
+
+			} catch (Exception e) {
+				ViewFuncs.showError("System Error!!! Contact Support.");
+				e.printStackTrace();
+				return;
+			} finally {
+				historyButton.setEnabled(true);
 			}
 		});
+		balancesButton.addClickListener(event -> {
+			try {
+				List<IsinActionVO> isinActionVOList = null;
+				
+				if (securitySearchComponent.getIsinTextField() == null || securitySearchComponent.getIsinTextField().isEmpty()) {
+					return;
+				}
+				isinActionVOList = debtEquityMutualService.determineBalancesMultiple(securitySearchComponent.getIsinTextField().getValue(), new java.sql.Date(new java.util.Date().getTime()), null);
+				Notification.show("No. of ISIN Actions with Balances: " + isinActionVOList.size())
+					.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+				isinActionsGrid.setColumns("settlementDate", "isin", "securityName", "isinActionId", "tradeId", "actionType", "bookingType", "dematAccount", "balance", "ppuBalance");
+				for (Column<IsinActionVO> column : isinActionsGrid.getColumns()) {
+					column.setResizable(true);
+				}
+				isinActionsGrid.setItems(isinActionVOList);
+			} catch (Exception e) {
+				ViewFuncs.showError("System Error!!! Contact Support.");
+				e.printStackTrace();
+				return;
+			} finally {
+				balancesButton.setEnabled(true);
+			}
+		});
+		
+	}
+	
+	private void handleMatchIsinActions(FormLayout formLayout) {
 		
 	}
 }
---
+++ src/main/java/org/sakuram/persmony/view/SecuritySearchComponent.java
@@ -0,0 +1,187 @@
+package org.sakuram.persmony.view;
+
+import java.util.List;
+import org.sakuram.persmony.service.DebtEquityMutualService;
+import org.sakuram.persmony.service.MiscService;
+import org.sakuram.persmony.util.Constants;
+import org.sakuram.persmony.util.UtilFuncs;
+import org.sakuram.persmony.valueobject.FieldSpecVO;
+import org.sakuram.persmony.valueobject.IdValueVO;
+import org.sakuram.persmony.valueobject.IsinCriteriaVO;
+import org.sakuram.persmony.valueobject.IsinVO;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.Grid.Column;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter(AccessLevel.NONE)
+public class SecuritySearchComponent {
+	HorizontalLayout layout;
+	TextField isinTextField;
+	
+	public SecuritySearchComponent(DebtEquityMutualService debtEquityMutualService, MiscService miscService) {
+		Button searchButton;
+		
+		layout = new HorizontalLayout();
+		isinTextField = new TextField("ISIN");
+		layout.add(isinTextField);
+		searchButton = new Button("Search");
+		layout.add(searchButton);
+		searchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		searchButton.setDisableOnClick(true);
+		// On click of Fetch
+		searchButton.addClickListener(event -> {
+			Dialog dialog;
+			Button closeButton;
+			VerticalLayout verticalLayout;
+			TextField isinTextField, companyNameTextField, securityNameTextField;
+			Select<IdValueVO> isinOperatorSelect, companyNameOperatorSelect, securityNameOperatorSelect;
+			Select<IdValueVO> securityTypeDvSelect;
+			HorizontalLayout hLayout;
+			Button fetchButton;
+			Grid<IsinVO> isinsGrid;
+			
+			try {
+				dialog = new Dialog();
+				dialog.setHeaderTitle("Security Search");
+				closeButton = new Button(new Icon("lumo", "cross"),
+				        (e) -> {
+				        	dialog.close();
+				        });
+				closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+				dialog.getHeader().add(closeButton);
+				verticalLayout = new VerticalLayout();
+				verticalLayout.getStyle().set("width", "75rem");
+				dialog.add(verticalLayout);
+				
+				isinOperatorSelect = ViewFuncs.newSelect(FieldSpecVO.getTxtOperatorList(), "Operator", true, false);
+				isinTextField = new TextField("Value");
+				hLayout = new HorizontalLayout();
+				verticalLayout.add(new FormLayout().addFormItem(hLayout, "ISIN"));
+				hLayout.add(isinOperatorSelect, isinTextField);
+				isinOperatorSelect.addValueChangeListener(valueChangEevent -> {
+					if (isinOperatorSelect.getValue() == null) {
+						isinTextField.setValue("");
+					}
+				});
+				
+				companyNameOperatorSelect = ViewFuncs.newSelect(FieldSpecVO.getTxtOperatorList(), "Operator", true, false);
+				companyNameTextField = new TextField("Value");
+				hLayout = new HorizontalLayout();
+				verticalLayout.add(new FormLayout().addFormItem(hLayout, "Company Name"));
+				hLayout.add(companyNameOperatorSelect, companyNameTextField);
+				companyNameOperatorSelect.addValueChangeListener(valueChangEevent -> {
+					if (companyNameOperatorSelect.getValue() == null) {
+						companyNameTextField.setValue("");
+					}
+				});
+				
+				securityNameOperatorSelect = ViewFuncs.newSelect(FieldSpecVO.getTxtOperatorList(), "Operator", true, false);
+				securityNameTextField = new TextField("Value");
+				hLayout = new HorizontalLayout();
+				verticalLayout.add(new FormLayout().addFormItem(hLayout, "Security Name"));
+				hLayout.add(securityNameOperatorSelect, securityNameTextField);
+				securityNameOperatorSelect.addValueChangeListener(valueChangEevent -> {
+					if (securityNameOperatorSelect.getValue() == null) {
+						securityNameTextField.setValue("");
+					}
+				});
+				
+				securityTypeDvSelect = ViewFuncs.newDvSelect(miscService, Constants.CATEGORY_SECURITY_TYPE, null, true, false);
+				verticalLayout.add(new FormLayout().addFormItem(securityTypeDvSelect, "Security Type"));
+				
+				fetchButton = new Button("Fetch");
+				verticalLayout.add(fetchButton);
+				fetchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+				fetchButton.setDisableOnClick(true);
+				
+				isinsGrid = new Grid<>(IsinVO.class);
+				isinsGrid.setColumns("isin", "companyName", "securityName", "securityType");
+				for (Column<IsinVO> column : isinsGrid.getColumns()) {
+					column.setResizable(true);
+				}
+				verticalLayout.add(isinsGrid);
+				
+				// On click of Fetch
+				fetchButton.addClickListener(clickEvent -> {
+					IsinCriteriaVO isinCriteriaVO;
+					List<IsinVO> isinVOList = null;
+
+					try {
+						// Validation
+						if (!isinTextField.isEmpty() && isinOperatorSelect.getValue() == null) {
+							ViewFuncs.showError("ISIN: Non-matching Operator and Value");
+							return;
+						}
+						if (isinTextField.isEmpty() && isinOperatorSelect.getValue() != null) {
+							ViewFuncs.showError("Specify Value for ISIN");
+							return;
+						}
+						if (!companyNameTextField.isEmpty() && companyNameOperatorSelect.getValue() == null) {
+							ViewFuncs.showError("Company Name: Non-matching Operator and Value");
+							return;
+						}
+						if (companyNameTextField.isEmpty() && companyNameOperatorSelect.getValue() != null) {
+							ViewFuncs.showError("Specify Value for Company Name");
+							return;
+						}
+						if (!securityNameTextField.isEmpty() && securityNameOperatorSelect.getValue() == null) {
+							ViewFuncs.showError("Security Name: Non-matching Operator and Value");
+							return;
+						}
+						if (securityNameTextField.isEmpty() && securityNameOperatorSelect.getValue() != null) {
+							ViewFuncs.showError("Specify Value for Security Name");
+							return;
+						}
+						
+						// Back-end Call
+						isinCriteriaVO = new IsinCriteriaVO(
+								isinTextField.getValue().equals("") ? null : isinTextField.getValue(),
+								isinOperatorSelect.getValue() == null ? null : isinOperatorSelect.getValue().getValue(),
+								companyNameTextField.getValue().equals("") ? null : companyNameTextField.getValue(),
+								companyNameOperatorSelect.getValue() == null ? null : companyNameOperatorSelect.getValue().getValue(),
+								securityNameTextField.getValue().equals("") ? null : securityNameTextField.getValue(),
+								securityNameOperatorSelect.getValue() == null ? null : securityNameOperatorSelect.getValue().getValue(),
+								securityTypeDvSelect.getValue() == null ? null : securityTypeDvSelect.getValue().getId()
+								);
+						try {
+							isinVOList = debtEquityMutualService.searchSecurities(isinCriteriaVO);
+							Notification.show("No. of ISINs fetched: " + isinVOList.size())
+								.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+							isinsGrid.setItems(isinVOList);
+						} catch (Exception e) {
+							ViewFuncs.showError(UtilFuncs.messageFromException(e));
+							return;
+						}
+					} finally {
+						fetchButton.setEnabled(true);
+					}
+				});
+				
+				isinsGrid.addItemDoubleClickListener(dcEvent -> {
+					this.isinTextField.setValue(dcEvent.getItem().getIsin());
+		        	dialog.close();
+				});
+				dialog.open();
+					
+			} finally {
+				searchButton.setEnabled(true);
+			}
+				
+		});
+	}
+}
